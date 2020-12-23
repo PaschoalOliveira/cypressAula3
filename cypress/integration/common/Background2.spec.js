49ddb06519c4ff5a05404ae8807d141f0bb33d77
Luma Lisley Figueiredo Guimaraes <luma.guimaraes@solutis.net.br>
2020-12-23 09:00:00 -0300

    editando arq denuncia

--- cypress/integration/common/Background2.spec.js
+++ cypress/integration/common/Background2.spec.js
@@ -1,14 +1,22 @@
 Given('acesso ao sistema da COELBA', () => {
     cy.visit('https://servicos.coelba.com.br/Pages/index.aspx')
 })
-
+And('acesso a seção de {string}',secao => {
+    cy.get('a').contains(secao)
+    .click({force:true})
+})
+And('acesso a seção de {string}',secao => {
+    cy.get('a').contains(secao)
+    .click({force:true})
+})
 And('preencher as informações', (dataTable) => {
+    //console.log(dataTable.hashes())
     dataTable.hashes().forEach(inf => {
-    cy.get('#end_logradouro').select(inf.Endereço)
-    cy.get('#end_numero').select(inf.Número)
-    cy.get('#end_bairro').select(inf.Bairro)
-    cy.get('#end_cidade').select(inf.Cidade)
-    cy.get('#ponto_referencia').select(inf.Ponto_de_Referência)
-    cy.get('#texto_denuncia').select(inf.Texto_para_Denúncia)
+        //cy.xpath('.//*[@id="end_logradouro"]').type(inf.Endereço)
+        cy.get('#end_numero').type(inf.Número)
+        cy.get('#end_bairro').type(inf.Bairro)
+        cy.get('#end_cidade').type(inf.Cidade)
+        cy.get('#ponto_referencia').type(inf.Ponto_de_Referência)
+        cy.get('#texto_denuncia').type(inf.Texto_para_Denúncia)
     })
 })
